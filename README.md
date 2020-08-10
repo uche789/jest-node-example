336d4ebd8e76c9f08dfa0bc8d1af12c2cfe5ef99
uche <thesimplearchitect@gmail.com>
2020-08-10 21:57:19 +0200

    Added ESLint

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ Example setup with Node, Jest and Babel.
 ### Packages
 - Babel
 - Jest
+- ESLint
+
+
+### Tips
+To install configuration for Jest and eslint, use the following commands: `npx jest --init`, `npx eslint --init`
--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 // https://github.com/babel/example-node-server
 
 const calculateSum = (array) => {
-  console.assert(Array.isArray(array), 'Paramter should be an array');
+  if (Array.isArray(array)) throw new TypeError('Paramter should be an array');
 
-  return array.reduce((acc, value) => {
-    return acc + value;
-  });
-}
+  return array.reduce((acc, value) => acc + value);
+};
 
 export default {
-  calculateSum
+  calculateSum,
 };
